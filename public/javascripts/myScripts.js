162aedeee7ee383dfb99e49531b7def83ecb06ae
Travis Elling <ellingtravis@gmail.com>
2017-09-21 10:44:33 -0700

    This is the designed portion of the new web app. Not yet responsive. Animated for 1370 x 720 res

--- public/javascripts/myScripts.js
+++ public/javascripts/myScripts.js
@@ -7,5 +7,18 @@ $('.welcome').animate({
     fontSize : "5em"
 },3000);
 $('.tagLine').fadeIn(9000);
+$('.contactHeader').animate({
+    fontSize : "2em"
+},3000);
+$('.formWrapper').animate({
+    right:"0px"
+},2000, "linear");
+$('.aboutSectionHeader').animate({
+    fontSize:"4em"
+},3000);
 
+$(".logo").click(function(){
+$(".menu").hide();
 });
+});
+
